f1cc79bc43e7ae4c4e2720435c652b8e1e3a3468
Pedro Guzman <pedro@subvertic.com>
2018-11-10 14:29:18 -0600

    Se agregó ejemplo de curriculum con diccionario

---
+++ Estructuras/curriculum_vitae.py
@@ -0,0 +1,51 @@
+curriculum = {
+    "name": "John", 
+    "last_name": "Smith",
+    "age": 37, 
+    "email": "john.smith@example.com",
+    "education": [
+        {
+            "instituion": "Super High School", 
+            "completed": True,
+            "degree": "Bachillerado en Educación Media", 
+            "year_of_completion": 2002
+        },
+        {
+            "instituion": "University of California in Berkeley", 
+            "completed": True,
+            "degree": "Bachellor Degree Applied Mathematics", 
+            "year_of_completion": 2006
+        },
+        {
+            "instituion": "Harvard University", 
+            "completed": True,
+            "degree": "Master of Science in Data Science", 
+            "year_of_completion": 2009
+        }
+    ],
+    "experience": [
+        {
+            "company": "GladOS Research Lab",
+            "position": "Staff Data Analyst",
+            "started": 2007,
+            "finished": 2008,
+            "actual_job": False,
+            "description": "Part of the staff working in diverse revenue leakage tasks"
+        },
+        {
+            "company": "Troogle Inc.",
+            "position": "Junior Social Data Analyst",
+            "started": 2009,
+            "finished": 2011,
+            "actual_job": False,
+            "description": "Designed algorithms to analyze people's social media and sell them stuff they don't need"
+        },
+        {
+            "company": "Tramazon Corporation",
+            "position": "Senior Evil Algorithm Designer",
+            "started": 2011,
+            "actual_job": True,
+            "description": "Lead the team of Automated Manipulation Algorithms Research Group"
+        }
+    ]
+}
