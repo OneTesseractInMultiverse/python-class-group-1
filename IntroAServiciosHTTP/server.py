aa53edb715756669f9ac3f0c52f00ac12e80a561
Pedro Guzman <pedro@subvertic.com>
2018-11-26 18:50:48 -0600

    Example api

--- IntroAServiciosHTTP/server.py
+++ IntroAServiciosHTTP/server.py
@@ -1,10 +1,23 @@
+"""
+Antes de ejecutar este programa, se debe instalar flask por medio del comando:
+pip install flask
+"""
 import os
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, jsonify
+
+
+app = Flask("EvilApi")
 
 @app.route('/')
 def get_root_resource():
     return 'This is a root resource'
+
+@app.route('/api/v1/person', methods=["GET"])
+def get_person():
+    return jsonify({
+        "name": "John", 
+        "last_name": "Smith"
+    }), 200
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
