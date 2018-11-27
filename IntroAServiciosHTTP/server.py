608bf82f4cb68b9e8abc16925410be37af91d62f
Pedro Guzman <pedro@subvertic.com>
2018-11-26 19:46:41 -0600

    Example server added

--- IntroAServiciosHTTP/server.py
+++ IntroAServiciosHTTP/server.py
@@ -3,21 +3,60 @@ Antes de ejecutar este programa, se debe instalar flask por medio del comando:
 pip install flask
 """
 import os
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 
 app = Flask("EvilApi")
+app.debug = True
+
+persons = []
+
+def id_exists(id, persons):
+    app.logger.debug("id_exists was called")
+    app.logger.debug(id)
+    app.logger.debug(persons)
+    for person in persons:
+        if person["id"] == id:
+            return True
+    return False
 
 @app.route('/')
 def get_root_resource():
     return 'This is a root resource'
 
+@app.route('/api/v1/person', methods=["POST"])
+def post_person():
+    # esto es una validación para garantizar que los datos vienen en json que es 
+    # lo que este servidor sabe interpretar.
+    if not request.is_json:
+        return jsonify({
+            "msg": "Only json is supported in this api"
+        }), 400
+    
+    # Si llegamos acá es porque los datos sí son JSON
+    datos = request.get_json()
+
+    # Para poder crear una persona (recurso) es mandatorio 
+    # que tenga un id único
+    if "id" not in datos:
+        return jsonify({
+            "msg": "An id is required"
+        }), 400
+
+    if id_exists(datos["id"], persons):
+        return jsonify({
+            "msg": "The provided id is already in use"
+        }), 400
+
+    persons.append(datos)
+
+    return jsonify({
+        "msg": "Person created"
+    }), 201
+
 @app.route('/api/v1/person', methods=["GET"])
 def get_person():
-    return jsonify({
-        "name": "John", 
-        "last_name": "Smith"
-    }), 200
+    return jsonify(persons), 200
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
